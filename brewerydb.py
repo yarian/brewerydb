2fef37ba88ea393ee4bd2fba9061667875022a04
Yarian Gomez <yarianyg@gmail.com>
2013-03-14 01:34:02 -0700

    Added search_upc method for search/upc endpoint

--- brewerydb.py
+++ brewerydb.py
@@ -4,17 +4,17 @@ DEFAULT_BASE_URI = "http://api.brewerydb.com/v2"
 BASE_URI = ""
 API_KEY = ""
 
-simple_endpoints = ["beers", "breweries", "categories", "events", 
+simple_endpoints = ["beers", "breweries", "categories", "events",
                     "featured", "features", "fluidsizes", "glassware",
                     "locations", "guilds", "heartbeat", "ingredients",
-                    "search", "socialsites", "styles"]
-    
+                    "search", "search/upc", "socialsites", "styles"]
+
 single_param_endpoints = ["beer", "brewery", "category", "event",
                           "feature", "glass", "guild", "ingredient",
                           "location", "socialsite", "style", "menu"]
 
 class BreweryDb:
-    
+
     @staticmethod
     def __make_simple_endpoint_fun(name):
         @staticmethod
@@ -33,14 +33,14 @@ class BreweryDb:
     def _get(request, options):
         options.update({"key" : BreweryDb.API_KEY})
         return requests.get(BreweryDb.BASE_URI + request, params=options).json()
-    
+
     @staticmethod
     def configure(apikey, baseuri=DEFAULT_BASE_URI):
         BreweryDb.API_KEY = apikey
         BreweryDb.BASE_URI = baseuri
         for endpoint in simple_endpoints:
             fun = BreweryDb.__make_simple_endpoint_fun(endpoint)
-            setattr(BreweryDb, endpoint, fun)
+            setattr(BreweryDb, endpoint.replace('/', '_'), fun)
         for endpoint in single_param_endpoints:
             fun = BreweryDb.__make_singlearg_endpoint_fun(endpoint)
-            setattr(BreweryDb, endpoint, fun)
+            setattr(BreweryDb, endpoint.replace('/', '_'), fun)
